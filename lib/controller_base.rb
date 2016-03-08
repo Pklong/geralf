be402e58a20e3b4db12004ea673ba881ee15b34c
PKLong <patrickklong@gmail.com>
2016-03-08 15:09:47 -0500

    finish phase 2 and 3, controller and erb template

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -1,5 +1,6 @@
 require 'active_support'
 require 'active_support/core_ext'
+require 'active_support/inflector'
 require 'erb'
 require_relative './session'
 
@@ -8,25 +9,51 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise if already_built_response?
+
+    res['location'] = url
+    res.status = 302
+    @already_built_response = true
+
+    res.finish
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise if already_built_response?
+
+    res['Content-Type'] = content_type
+    res.write(content)
+    @already_built_response = true
+
+    res.finish
+
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    raw_template = File.read(
+      "views/" +
+      "#{self.class}".underscore +
+      "/#{template_name}.html.erb"
+      )
+    template = ERB.new(raw_template).result(binding)
+
+    render_content(template, "text/html")
   end
 
   # method exposing a `Session` object
@@ -37,4 +64,3 @@ class ControllerBase
   def invoke_action(name)
   end
 end
-
