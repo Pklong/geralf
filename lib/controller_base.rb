9e78045fe2b5d25fdc432f10c0270736e5bfe944
PKLong <patrickklong@gmail.com>
2016-04-17 22:36:41 -0400

    clean-up extraneous files

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -7,19 +7,16 @@ require_relative './session'
 class ControllerBase
   attr_reader :req, :res, :params
 
-  # Setup the controller
   def initialize(req, res, route_params = {})
     @req = req
     @res = res
     @params = req.params.merge(route_params)
   end
 
-  # Helper method to alias @already_built_response
   def already_built_response?
     !!@already_built_response
   end
 
-  # Set the response status code and header
   def redirect_to(url)
     raise if already_built_response?
 
@@ -31,9 +28,6 @@ class ControllerBase
     res.finish
   end
 
-  # Populate the response with content.
-  # Set the response's content type to the given type.
-  # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
     raise if already_built_response?
 
@@ -42,11 +36,8 @@ class ControllerBase
     @already_built_response = true
     session.store_session(res)
     res.finish
-
   end
 
-  # use ERB and binding to evaluate templates
-  # pass the rendered html to render_content
   def render(template_name)
     raw_template = File.read(
       "views/" +
@@ -58,12 +49,10 @@ class ControllerBase
     render_content(template, "text/html")
   end
 
-  # method exposing a `Session` object
   def session
     @session ||= Session.new(req)
   end
 
-  # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
     self.send(name)
     render(name) unless @already_built_response
--- lib/session.rb
+++ lib/session.rb
@@ -3,7 +3,6 @@ require 'json'
 
 class Session
   APP = '_rails_lite_app'
-  # deserialize the cookie into a hash
   def initialize(req)
     @store = (req.cookies[APP] ? JSON.parse(req.cookies[APP]) : {})
   end
@@ -16,13 +15,9 @@ class Session
     @store[key] = val
   end
 
-  # serialize the hash into json and save in a cookie
-  # add to the responses cookies
   def store_session(res)
 
     @store[:path] = "/"
-
     res.set_cookie(APP, @store.to_json)
   end
-
 end
